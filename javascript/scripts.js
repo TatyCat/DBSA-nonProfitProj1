c0340205575783ecc762e5ff751dba20ce37e2c5
Tatyanna Cobb <developer-tatyanna@rrcs-24-73-174-158.se.biz.rr.com>
2018-10-22 19:52:50 -0400

    First Commit. Nav Bar updated

---
+++ javascript/scripts.js
@@ -0,0 +1,9 @@
+
+function responsiveMenu() {
+    /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+    
+    const nav = document.querySelector("#topNavBarID");
+    nav.classList.toggle("responsive")
+
+}
+
