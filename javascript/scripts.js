47beaa5f37c2196ea50fd98f245ce8e7c3118bd7
Tatyanna Cobb <developer-tatyanna@Tatyannas-MacBook-Pro.local>
2018-10-23 12:38:33 -0400

    90% finished debugging the nav menu bar

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,9 +1,7 @@
 
 function responsiveMenu() {
     /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-    
     const nav = document.querySelector("#topNavBarID");
     nav.classList.toggle("responsive")
-
 }
 
